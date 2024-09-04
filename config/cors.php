0ce526fd836599df97095e60ad67056a13fd1828
Younes lassiri <youneslassirifreelance@gmail.com>
2024-09-04 14:01:26 +0100

    'path'

--- config/cors.php
+++ config/cors.php
@@ -15,15 +15,15 @@ return [
     |
     */
 
-    'paths' => ['*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*','api/api/*', 'sanctum/csrf-cookie','*'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['POST','GET','DELETE','PUT','*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000/','*'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['X-Custom-Header', 'Upgrade-Insecure-Requests', '*'],
 
     'exposed_headers' => [],
 
